01e9c1759ec44290a944201a0d300646785b5819
Juan <jj.monte2002@gmail.com>
2025-02-07 22:28:08 -0500

    First commit

--- conexion.php
+++ conexion.php
@@ -1,1 +1,12 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "induccion_bd";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+?>
